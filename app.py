1a6f4252bc308a6a3efd9137a80e9c2db746f9cc
Ioan Hadarean <hadareannelutu@yahoo.com>
2019-02-13 13:38:15 +0000

    Imported wtforms, fields and validators/added register form class

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import os
-from flask import Flask, redirect, render_template, request, url_for, flash, session
+from flask import Flask, redirect, render_template, request, url_for, flash, session, logging
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
+
 
 app = Flask(__name__)
 app.secret_key = os.getenv("SECRET", "1403goagl")
@@ -24,14 +28,24 @@ def index():
     
 @app.route('/index.html/<username>', methods = ["GET", "POST"])
 def user(username):
-    """Add and display chat messages"""
+    """Login for user"""
     
     if request.method == "POST":
         username = session["username"]
         return redirect(url_for("user", username = session["username"]))
         
     return render_template("index.html", username = username)
-   
+    
+class RegisterForm(Form):
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email', [validators.Length(min=6, max=50)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords do not match')
+        ])
+    confirm = PasswordField('Confirm Password')
+
 if __name__ == "__main__":
     app.run(host=os.environ.get('IP'),
         port=int(os.environ.get('PORT')),
