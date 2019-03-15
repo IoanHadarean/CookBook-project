74203eb473354b6dfe0a898f5c78c690bedea573
Ioan Hadarean <hadareannelutu@yahoo.com>
2019-03-15 23:39:16 +0000

    Fixed undefined random welcome message

--- static/js/welcome.js
+++ static/js/welcome.js
@@ -1,6 +1,10 @@
-const messages = ["Vegan CookBook is super excited to have you onboard", "Doesn't that seem delicious? Try it out",
-                 "We predict that in the next minute you will go get something to eat", "There you go, welcome to our food fiesta"];
-                 
-const randomIndex = Math.round(Math.random()*messages.length);
+window.onload = function(e) {
+    const messages = ["Vegan CookBook is super excited to have you onboard", "Doesn't that seem delicious? Try it out",
+        "We predict that in the next minute you will go get something to eat", "There you go, welcome to our food fiesta"
+    ];
 
-document.getElementsByClassName("random-message")[0].innerHTML = messages[randomIndex];
+    const randomIndex = Math.floor(Math.random() * messages.length);
+
+    document.getElementsByClassName("random-message")[0].innerHTML = messages[randomIndex];
+    e.preventDefault();
+};
