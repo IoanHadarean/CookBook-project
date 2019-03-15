7fd390664346bc67a590ad9727488117002a5380
Ioan Hadarean <hadareannelutu@yahoo.com>
2019-03-15 23:29:21 +0000

    Added random welcoming message

---
+++ static/js/welcome.js
@@ -0,0 +1,6 @@
+const messages = ["Vegan CookBook is super excited to have you onboard", "Doesn't that seem delicious? Try it out",
+                 "We predict that in the next minute you will go get something to eat", "There you go, welcome to our food fiesta"];
+                 
+const randomIndex = Math.round(Math.random()*messages.length);
+
+document.getElementsByClassName("random-message")[0].innerHTML = messages[randomIndex];
