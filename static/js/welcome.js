8d8ddc845fe33626122ab6159e8b603bcd93b1e3
Ioan Hadarean <hadareannelutu@yahoo.com>
2019-03-18 16:49:22 +0000

    Added event listener on resize

--- static/js/welcome.js
+++ static/js/welcome.js
@@ -1,4 +1,5 @@
 window.onload = function() {
+
     const messages = ["WorldWide CookBook is super excited to have you onboard", "Doesn't that seem delicious? Try it out",
         "We predict that in the next minute you will go get something to eat", "There you go, welcome to our food fiesta!",
         "Take a look at these delicious recipes and get that frying pan out of the cupboard",
@@ -7,16 +8,37 @@ window.onload = function() {
         "How are you going to resist? The answer is, you cannot",
         "For people that love to cook and try new things out"
     ];
-    
-    const colors = ["#00FFFF", "#FFA57D", "#FFC0CB"];
 
+    let colors = ["#00FFFF", "#FFA57D", "#FFC0CB", "#8A2BE2","#ADD8E6"];
     const randomIndexMessages = Math.floor(Math.random() * messages.length);
     const randomIndexColors = Math.floor(Math.random() * colors.length);
-    
+
     let welcomeHeader = document.getElementsByClassName("random-message")[0];
 
     welcomeHeader.innerHTML = messages[randomIndexMessages];
     welcomeHeader.style.backgroundColor = colors[randomIndexColors];
+
+    window.addEventListener('resize', randomColors);
+
+    function randomColors() {
+        if (screen.width <= 560) {
+            let colorsMobile = ["#8A2BE2","#ADD8E6"];
+            let welcomeHeaderMobile = document.getElementsByClassName("random-message")[0];
+            const randomIndexMessagesMobile = Math.floor(Math.random() * messages.length);
+            const randomIndexColorsMobile = Math.floor(Math.random() * colorsMobile.length);
+            welcomeHeaderMobile.innerHTML = messages[randomIndexMessagesMobile];
+            welcomeHeaderMobile.style.backgroundColor = colorsMobile[randomIndexColorsMobile];
+        }
+        else if (screen.width > 560) {
+            let colorsDesktop = ["#00FFFF", "#FFA57D", "#FFC0CB"];
+            const randomIndexMessagesDesktop = Math.floor(Math.random() * messages.length);
+            const randomIndexColorsDesktop = Math.floor(Math.random() * colorsDesktop.length);
+
+            let welcomeHeaderDesktop = document.getElementsByClassName("random-message")[0];
+
+            welcomeHeaderDesktop.innerHTML = messages[randomIndexMessagesDesktop];
+            welcomeHeaderDesktop.style.backgroundColor = colorsDesktop[randomIndexColorsDesktop];
+        }
+    }
+
 };
-
-
