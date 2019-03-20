b2e24a8461f76f40379503ef7e174f0cde70b4ac
Ioan Hadarean <hadareannelutu@yahoo.com>
2019-03-20 17:01:10 +0000

    Added fix for scripts/addes small css fixes

---
+++ static/js/navbar.js
@@ -0,0 +1,42 @@
+// Load the content
+
+window.onload = function() {
+
+    // Get all links
+    let navLinks = document.querySelectorAll('li a');
+
+    // Iterate through links
+    for (let i = 0; i < navLinks.length; i++) {
+
+        // Customize login link
+        if (navLinks[i].textContent == 'Login') {
+            navLinks[i].style.backgroundColor = '#f26226';
+            navLinks[i].style.height = '30px';
+            navLinks[i].style.marginTop = '10px';
+            navLinks[i].style.color = 'white';
+            navLinks[i].style.display = 'flex';
+            navLinks[i].style.alignItems = 'center';
+            navLinks[i].style.justifyContent = 'center';
+        }
+
+        // Customize register link
+        else if (navLinks[i].textContent == 'Register') {
+            navLinks[i].style.backgroundColor = 'white';
+            navLinks[i].style.height = '30px';
+            navLinks[i].style.marginTop = '10px';
+            navLinks[i].style.color = 'black';
+            navLinks[i].style.display = 'flex';
+            navLinks[i].style.alignItems = 'center';
+            navLinks[i].style.justifyContent = 'center';
+        }
+
+
+        //Customize other links
+
+        else {
+            navLinks[i].style.display = 'flex';
+            navLinks[i].style.alignItems = 'center';
+            navLinks[i].style.justifyContent = 'center';
+        }
+    }
+};
---
+++ static/js/random_message.js
@@ -0,0 +1,34 @@
+// Load the content
+window.onload = function() {
+    // Added random welcome messages and colors array
+    // Added randomIndex to each of the messages and colors
+    // Colors array changes depending on the device width
+
+    const messages = ["WorldWide CookBook is super excited to have you onboard", "Doesn't that seem delicious? Try it out",
+        "We predict that in the next minute you will go get something to eat", "There you go, welcome to our food fiesta!",
+        "Take a look at these delicious recipes and get that frying pan out of the cupboard",
+        "Is creativity your number one trait? Then you got the perfect matching recipes",
+        "A recipe a day keeps the hunger away", "Recipes with love for your entire family!",
+        "How are you going to resist? The answer is, you cannot",
+        "For people that love to cook and try new things out"
+    ];
+
+    if (screen.width <= 560) {
+        let colorsMobile = ["#FFFFFF", "#ADD8E6"];
+        let welcomeHeaderMobile = document.getElementsByClassName("random-message")[0];
+        const randomIndexMessagesMobile = Math.floor(Math.random() * messages.length);
+        const randomIndexColorsMobile = Math.floor(Math.random() * colorsMobile.length);
+        welcomeHeaderMobile.innerHTML = messages[randomIndexMessagesMobile];
+        welcomeHeaderMobile.style.backgroundColor = colorsMobile[randomIndexColorsMobile];
+    }
+    else if (screen.width > 560) {
+        let colorsDesktop = ["#E8E8E8", "#FFFFFF", "#ADD8E6"];
+        const randomIndexMessagesDesktop = Math.floor(Math.random() * messages.length);
+        const randomIndexColorsDesktop = Math.floor(Math.random() * colorsDesktop.length);
+
+        let welcomeHeaderDesktop = document.getElementsByClassName("random-message")[0];
+
+        welcomeHeaderDesktop.innerHTML = messages[randomIndexMessagesDesktop];
+        welcomeHeaderDesktop.style.backgroundColor = colorsDesktop[randomIndexColorsDesktop];
+    }
+};
