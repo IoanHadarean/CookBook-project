7a8e3c5b769ef5d8b51f806ec068c114c55c016b
Ioan Hadarean <hadareannelutu@yahoo.com>
2019-03-27 23:08:24 +0000

    Added function for handling rating star percentage

--- static/js/ratings.js
+++ static/js/ratings.js
@@ -1,1 +1,21 @@
-// Initial ratings
+// Initial rating
+const rating = 0;
+
+// Total stars
+const starsTotal = 5;
+
+// Run getRatings when DOM loads
+document.addEventListener('DOMContentLoaded', getRatings);
+
+// Get ratings
+function getRatings() {
+    // Get percetage
+    const starPercentage = (rating / starsTotal) * 100;
+    
+    //Round to nearest 10
+    const starPercentageRounded = `${Math.round(starPercentage / 10) * 10}%`;
+    
+    //Set width of stars inner to percentage
+    document.querySelector('.stars-inner').style.width = starPercentageRounded;
+    
+}
