4f963d6e3e7e6cd7d73b5708f7b384717488d75c
Ioan Hadarean <hadareannelutu@yahoo.com>
2019-04-11 13:35:37 +0000

    Fixed footer behaviour for search page

--- static/js/search.js
+++ static/js/search.js
@@ -3,15 +3,17 @@ var searchButton = document.getElementById('submit_search');
 var form = document.getElementById('search_form');
 var input = document.getElementById('search_input');
 var footerTag = document.getElementsByTagName('footer')[0];
+var results = document.getElementsByClassName('container')[0];
+var recipes = document.getElementsByClassName('recipes')[0];
 
 // Load the search results
 if (input) {
     input.addEventListener('input', function loadResults() {
         let xhr = new XMLHttpRequest();
-        let inputValue = document.getElementById('search_input').value;
-        console.log(inputValue);
+        var inputValue = document.getElementById('search_input').value;
         document.getElementById('search_message').innerHTML = '';
         document.getElementsByClassName('container')[0].innerHTML = '';
+        footerTag.style.position = 'absolute';
         inputValue = '';
         xhr.onload = function() {
             inputValue = '';
@@ -40,4 +42,10 @@ if (input) {
         xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
         xhr.send();
     });
+}
+
+if (results) {
+    if (results.contains(recipes) || input.value != '') {
+        footerTag.style.position = 'relative';
+    }
 }
