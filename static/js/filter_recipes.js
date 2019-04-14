7788c260feb61f6d3f581e32f044bad3721069a2
Ioan Hadarean <hadareannelutu@yahoo.com>
2019-04-14 23:48:08 +0000

    Managed to get the AJAX filter request working/need to fix bug with empty array

--- static/js/filter_recipes.js
+++ static/js/filter_recipes.js
@@ -36,15 +36,17 @@ function num_results() {
     for (let i = 0; i <parsedSelects.length; i++) {
         options[parsedSelects[i].name] = parsedSelects[i].value;
     }
+    console.log(options);
     let xhr = new XMLHttpRequest();
     xhr.onload = function() {
             if (this.readyState === 4 && this.status === 200) {
                 let response = xhr.responseText;
+                console.log(response);
             }
     };
-    xhr.open("POST", "/filter_recipes", true);
+    xhr.open("POST", "/filter_results", true);
     xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhr.send();
+    xhr.send(options);
 }
 
 
