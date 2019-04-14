10e6985047c1edd5ce2527fa9e70cfd38d6ec198
Ioan Hadarean <hadareannelutu@yahoo.com>
2019-04-14 01:44:27 +0000

    Added responsive web design for search

--- static/js/search.js
+++ static/js/search.js
@@ -21,37 +21,39 @@ var countSpan = document.getElementById('search_message');
 
 // Load the search results
 if (input) {
-    input.addEventListener('input', function loadResults() {
+    input.addEventListener('input', function loadResults(e) {
         let xhr = new XMLHttpRequest();
         // var inputValue = document.getElementById('search_input').value;
         document.getElementById('search_message').innerHTML = '';
         document.getElementsByClassName('container')[0].innerHTML = '';
         footerTag.style.position = 'absolute';
-        inputValue = '';
         document.getElementsByClassName('container')[0].innerHTML = '';
         xhr.onload = function() {
-            inputValue = '';
             if (this.readyState === 4 && this.status === 200) {
                 let response = xhr.responseText;
+                console.log(response);
                 document.getElementById('search_message').innerHTML = '';
-                inputValue = '';
                 document.getElementsByClassName('container')[0].innerHTML = '';
+                if (response == 0) {
+                    
+                }
             }
             else {
                 console.log("Response not received");
                 document.getElementById('search_message').innerHTML = '';
-                inputValue = '';
                 document.getElementsByClassName('container')[0].innerHTML = '';
             }
         };
         xhr.open("POST", "/search_results", true);
         xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
         xhr.send();
+        e.preventDefault();
     });
 }
 
 if (results) {
-    if (results.contains(recipes) || input.value != '') {
+    if (results.contains(recipes)) {
         footerTag.style.position = 'relative';
     }
 }
+
