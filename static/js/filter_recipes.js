0b7d9f9a69d9d69da14b34db35e145115397bebf
Ioan Hadarean <hadareannelutu@yahoo.com>
2019-04-14 23:17:38 +0000

    Added AJAX request for filters

--- static/js/filter_recipes.js
+++ static/js/filter_recipes.js
@@ -3,17 +3,49 @@ let filterButton = document.getElementById('tags_search_btn');
 
 let selects = document.querySelectorAll('select');
 selects.forEach(select => select.onchange = function() {
-    console.log(select);
     return num_results();
 });
 
+
+
+
+
+
+
+
+
+
 function num_results() {
     filterButton.innerHTML = 'Processing...';
-    let selectedOption = [];
-    selects.forEach(select => selectedOption.push(select.options[select.selectedIndex]));
-    console.log(selectedOption);
+    let selectedOptions = [];
+    selects.forEach(select => selectedOptions.push(select.options[select.selectedIndex]));
+
+    let constraints = ['choose-allergens', 'choose-cuisines', 'choose-courses'];
+    let parsedSelects = selectedOptions.filter(parsed => !(constraints.includes(parsed.id)));
+    console.log(parsedSelects.length);
+    if (parsedSelects.length == 0) {
+        document.getElementById('results').innerHTML = '';
+        document.getElementById('num-results').innerHTML = '';
+        document.getElementById('tags_search_btn').innerHTML = 'Select a tag!';
+        return false;
+    }
+    else {
+        document.getElementById('tags_search_btn').disabled = false;
+    }
+    let options = {};
+    for (let i = 0; i <parsedSelects.length; i++) {
+        options[parsedSelects[i].name] = parsedSelects[i].value;
+    }
+    let xhr = new XMLHttpRequest();
+    xhr.onload = function() {
+            if (this.readyState === 4 && this.status === 200) {
+                let response = xhr.responseText;
+            }
+    };
+    xhr.open("POST", "/filter_recipes", true);
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.send();
 }
 
-// let selectedOption = [];
-// selects.forEach(select => selectedOption.push(select.options[select.selectedIndex]));
-// console.log(selectedOption);
+
+
