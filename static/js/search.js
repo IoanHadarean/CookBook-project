523806d85e8e90a12b67589017620516e7612d20
Ioan Hadarean <hadareannelutu@yahoo.com>
2019-04-28 21:06:06 +0000

    Added search results on active input typing/performed input checks/refactored code

--- static/js/search.js
+++ static/js/search.js
@@ -4,32 +4,99 @@ let footerTag = document.getElementsByTagName('footer')[0];
 let results = document.getElementsByClassName('container')[0];
 let recipes = document.getElementsByClassName('recipes')[0];
 let searchMessage = document.getElementById('search_message');
-let searchBtn = document.getElementById('submit_search');
+let searchBtn = document.getElementById('btnSearch');
 let btnContainer = document.getElementById('btn-container');
+let searchForm = document.getElementById('search_form');
+
+
+
+loadEventListeners();
+
+
+// Add Event Listeners
+function loadEventListeners() {
+    if (input) {
+        input.addEventListener('input', loadResults);
+        input.addEventListener('input', getInputResults);
+    }
+    if (searchBtn) {
+        searchBtn.addEventListener('click', getResults);
+        searchBtn.addEventListener('click', searchState);
+    }
+    document.addEventListener('click', styleDisabledButton);
+}
 
 
 // Load the search results
-if (input) {
-    input.addEventListener('input', function loadResults() {
-        if (searchMessage) {
-            searchMessage.innerHTML = '';
-        }
-        if (input.value.length >= 3) {
-            searchBtn.disabled = false;
+function loadResults() {
+    if (searchMessage) {
+        searchMessage.innerHTML = '';
+    }
+    if (input.value.length >= 3) {
+        searchBtn.disabled = false;
+    }
+    else {
+        searchBtn.disabled = true;
+    }
+    document.getElementsByClassName('container')[0].innerHTML = '';
+    footerTag.style.position = 'absolute';
+}
+
+
+// Get search results on click
+function getResults() {
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "/search_results/" + input.value, true);
+    xhr.onload = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            let response = this.responseText;
+            console.log(response);
+            document.getElementById('search_message').innerHTML = response;
         }
         else {
-            searchBtn.disabled = true;
+            console.log("Response not received");
         }
-        document.getElementsByClassName('container')[0].innerHTML = '';
-        footerTag.style.position = 'absolute';
-    });
+    };
+    xhr.onerror = function() {
+        console.log('Request error...');
+    };
+    xhr.send();
 }
 
 
-if (searchBtn) {
-    searchBtn.addEventListener('click', searchState);
+
+// Get number of search results on input
+function getInputResults() {
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "/search_results/" + input.value, true);
+    xhr.onload = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            let results = this.responseText;
+            console.log(results);
+            if (results === "0") {
+                document.getElementById('search_message').innerHTML = 'No recipes found';
+            }
+            else {
+                if (results === "1") {
+                    document.getElementById('search_message').innerHTML = results + ' recipe was found';
+                }
+                else {
+                    document.getElementById('search_message').innerHTML = results + ' recipes were found';
+                }
+            }
+            if (input.value === '') {
+                document.getElementById('search_message').innerHTML = '';
+            }
+        }
+    };
+    xhr.onerror = function() {
+        console.log('Request error...');
+    };
+    xhr.send();
 }
 
+
+// Set the state of the search button
 function searchState() {
     if (input.value.length >= 3) {
         searchBtn.disabled = false;
@@ -41,15 +108,15 @@ function searchState() {
 
 
 
-
-
+// Set the footer position to relative when there are results
 if (results) {
     if (results.contains(recipes)) {
         footerTag.style.position = 'relative';
     }
 }
 
-document.addEventListener('click', function(e) {
+// Style the search button when it's disabled
+function styleDisabledButton() {
     if (btnContainer) {
         let clickInside = btnContainer.contains(event.target);
         if (clickInside) {
@@ -60,4 +127,4 @@ document.addEventListener('click', function(e) {
             searchBtn.style.borderColor = 'white';
         }
     }
-});
+}
