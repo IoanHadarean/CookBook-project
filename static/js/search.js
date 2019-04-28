cbbbb427b16eff10aa926ea2a639b8d6945de6c9
Ioan Hadarean <hadareannelutu@yahoo.com>
2019-04-28 19:20:19 +0000

    Added number of results for search form on active input typing

--- static/js/search.js
+++ static/js/search.js
@@ -1,11 +1,16 @@
+/* global fetch */
+
+
 // Get HTML elements
 let input = document.getElementById('search_input');
 let footerTag = document.getElementsByTagName('footer')[0];
 let results = document.getElementsByClassName('container')[0];
 let recipes = document.getElementsByClassName('recipes')[0];
 let searchMessage = document.getElementById('search_message');
-let searchBtn = document.getElementById('submit_search');
+let searchBtn = document.getElementById('btnSearch');
 let btnContainer = document.getElementById('btn-container');
+let searchForm = document.getElementById('search_form');
+var searchText;
 
 
 // Load the search results
@@ -25,6 +30,63 @@ if (input) {
     });
 }
 
+// Get search text from input
+var getSearchText = searchText;
+console.log(typeof (getSearchText));
+
+
+
+if (searchBtn) {
+    searchBtn.addEventListener('click', getResults);
+}
+
+
+// Get search results on click
+function getResults() {
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "/search_results", true);
+    xhr.onload = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            let response = this.responseText;
+            console.log(response);
+            document.getElementById('search_message').innerHTML = response;
+        }
+        else {
+            console.log("Response not received");
+        }
+    };
+    xhr.onerror = function() {
+        console.log('Request error...');
+    };
+    xhr.send();
+}
+
+
+if(input) {
+    input.addEventListener('input', getInputResults);
+}
+
+// Get search results on input
+function getInputResults() {
+    let xhr = new XMLHttpRequest();
+    console.log(input.value);
+    xhr.open("POST", "/search_results/" + input.value, true);
+    xhr.onload = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            let response = this.responseText;
+            console.log(response);
+            document.getElementById('search_message').innerHTML = response;
+        }
+        else {
+            console.log("Response not received");
+        }
+    };
+    xhr.onerror = function() {
+        console.log('Request error...');
+    };
+    xhr.send();
+}
+
 
 if (searchBtn) {
     searchBtn.addEventListener('click', searchState);
@@ -33,6 +95,7 @@ if (searchBtn) {
 function searchState() {
     if (input.value.length >= 3) {
         searchBtn.disabled = false;
+        document.getElementById('search_form').submit();
     }
     else {
         searchBtn.disabled = true;
