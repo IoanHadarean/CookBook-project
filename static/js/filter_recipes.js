4f6ddecc52509479de67c52e424c243f4b0af4a4
Ioan Hadarean <hadareannelutu@yahoo.com>
2019-04-29 04:24:09 +0000

    Added array of dictionaries for the select values

--- static/js/filter_recipes.js
+++ static/js/filter_recipes.js
@@ -8,10 +8,8 @@ let filterRecipes = document.getElementsByClassName('recipes')[0];
 let filterResults = document.getElementById('num-results');
 
 
+
 // Add on change event for selects
-
-
-
 selects.forEach(select => select.onchange = function() {
     container.innerHTML = '';
     footer.style.position = 'absolute';
@@ -19,42 +17,55 @@ selects.forEach(select => select.onchange = function() {
     if (filterResults) {
         filterResults.innerHTML = '';
     }
-    let fullOptions = [];
-    let selectedOptions = [];
-    selects.forEach(select => selectedOptions.push(select.options[select.selectedIndex]));
-    console.log(selectedOptions);
-    let constraints = ['choose-allergens', 'choose-cuisines', 'choose-courses'];
-    let parsedSelects = selectedOptions.filter(parsed => !(constraints.includes(parsed.id)));
-    console.log(parsedSelects);
-    for (let i = 0; i < parsedSelects.length; i++) {
-        let options = {};
-        options[parsedSelects[i].name] = parsedSelects[i].value;
-        fullOptions.push(options);
-    }
-    console.log(fullOptions);
 });
 
-// filterButton.addEventListener('click', getFilterResults);
+
+// Adding a list of dictionaries for select options
+var fullOptions = [{ "allergen_name": "" }, { "cuisine_name": "" }, {"course_name": "" }];
+selects.forEach(select => select.onchange = function() {
+    let selectedOptions = [];
+    selectedOptions.push(select.options[select.selectedIndex]);
+    let constraints = ['choose-allergens', 'choose-cuisines', 'choose-courses'];
+    let parsedSelects = selectedOptions.filter(parsed => !(constraints.includes(parsed.id)));
+    if (select.id == 'course_name') {
+        for (let i = 0; i < parsedSelects.length; i++) {
+            fullOptions[2]["course_name"] = parsedSelects[i].value;
+        }
+    }
+    if (select.id == 'cuisine_name') {
+        for (let i = 0; i < parsedSelects.length; i++) {
+            fullOptions[1]["cuisine_name"] = parsedSelects[i].value;
+        }
+    }
+    if (select.id == 'allergen_name') {
+        for (let i = 0; i < parsedSelects.length; i++) {
+            fullOptions[0]["allergen_name"] = parsedSelects[i].value;
+        }
+    }
+});
 
 
 
 
 
-// function getFilterResults() {
-//     let xhr = new XMLHttpRequest();
-//     xhr.open("POST", "/filter_results/" + options, true);
-//     xhr.onload = function() {
-//         if (this.readyState === 4 && this.status === 200) {
-//             let response = xhr.responseText;
-//             console.log(response);
-//         }
-//     };
-//     xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-//     xhr.onerror = function() {
-//         console.log('Request error...');
-//     };
-//     xhr.send();
-// }
+
+
+
+function getFilterResults() {
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "/filter_results/" + fullOptions, true);
+    xhr.onload = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            let response = xhr.responseText;
+            console.log(response);
+        }
+    };
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.onerror = function() {
+        console.log('Request error...');
+    };
+    xhr.send();
+}
 
 
 // Fix footer according to results
@@ -67,6 +78,5 @@ if (container) {
 if (filterResults) {
     if (filterResults.innerHTML == 'No recipes found') {
         footer.style.position = 'absolute';
-        filterButton.disabled = false;
     }
 }
