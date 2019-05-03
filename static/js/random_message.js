0519552be8172e34896eeb6488ce089f614143c8
Ioan Hadarean <hadareannelutu@yahoo.com>
2019-05-03 03:54:05 +0000

    Added responsive web design for filter form and filter results

--- static/js/random_message.js
+++ static/js/random_message.js
@@ -4,7 +4,7 @@
 
 const messages = ["WorldWide CookBook is super excited to have you onboard", "Doesn't that seem delicious? Try it out",
     "We predict that in the next minute you will go get something to eat", "There you go, welcome to our food fiesta!",
-    "Take a look at these delicious recipes and get that frying pan out of the cupboard",
+    "Look at these delicious recipes! Get that frying pan out of the cupboard!",
     "Is creativity your number one trait? Then you got the perfect matching recipes",
     "A recipe a day keeps the hunger away", "Recipes with love for your entire family!",
     "How are you going to resist? The answer is, you cannot",
