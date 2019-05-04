685e9a8cd29ea1104787faba04a52b63bd8b53c9
Ioan Hadarean <hadareannelutu@yahoo.com>
2019-05-04 10:45:01 +0000

    Added checks for clearing localStorage

--- static/js/clearStorage.js
+++ static/js/clearStorage.js
@@ -1,27 +1,47 @@
  /* global localStorage */
 
 
-
+ // Clear local storage when user logs out, logs in or register
+ // to prevent users from getting multiple ingredient inputs or instruction inputs
+ // Note: even if not all the code below might be necessary, it's better to be preventive
 
  // Get HTML elements
  let loginNavbarButton = document.getElementById('login-button');
  let registerNavbarButton = document.getElementById('register-button');
  let logoutNavbarButton = document.getElementById('logout-button');
+ let loginFormButton = document.getElementById('login-form-button');
+ let registerFormButton = document.getElementById('register-form-button');
+ let editRecipeButtons = document.getElementsByClassName('edit-button');
+ let addRecipeForm = document.getElementById('add-recipe');
+ let editRecipeForm = document.getElementById('edit-recipe');
+
+ // Add event listeners for buttons, except for add and update recipe buttons
+ let buttonsArray = [loginNavbarButton, registerNavbarButton, logoutNavbarButton, loginFormButton, registerFormButton];
+ for (var i = 0; i < buttonsArray.length; i++) {
+  if (buttonsArray[i]) {
+   buttonsArray[i].addEventListener('click', clearLocalInformation);
+  }
+ }
 
 
- // Clear local storage when user logs out, logs in or register
- // to prevent users from getting multiple ingredient inputs or instruction inputs
- if (loginNavbarButton) {
-     loginNavbarButton.addEventListener('click', clearLocalInformation);
+ // Clear local storage when the add and edit recipe forms are submitted
+ if (addRecipeForm) {
+  addRecipeForm.addEventListener('submit', clearLocalInformation);
  }
- if (registerNavbarButton) {
-     registerNavbarButton.addEventListener('click', clearLocalInformation);
- }
- if (logoutNavbarButton) {
-     logoutNavbarButton.addEventListener('click', clearLocalInformation);
+ if (editRecipeForm) {
+  editRecipeForm.addEventListener('submit', clearLocalInformation);
  }
 
+ // Clear local storage when any edit button is clicked
+ if (editRecipeButtons) {
+  for (var i = 0; i < editRecipeButtons.length; i++) {
+   editRecipeButtons[i].addEventListener('click', clearLocalInformation);
+  }
+ }
+
+
+ // Clear local storage function
  function clearLocalInformation() {
-     localStorage.clear();
+  localStorage.clear();
  }
  
