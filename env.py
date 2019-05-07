86210aca6f25671571f334cce02304ed0ccb945a
Ioan Hadarean <hadareannelutu@yahoo.com>
2019-05-07 16:44:47 +0000

    Refactored env.py

--- env.py
+++ env.py
@@ -1,11 +1,10 @@
 import os
 
-# Create a random secret key
-secret = "HadareanIoan1403Goagl"
+# Create a secret key
+os.environ["SECRET_KEY"] = "HadareanIoan1403Goagl"
 # Storing the MONGO_URI environment variable
 os.environ["MONGO_URI"] = "mongodb://me:1403Goagl@ds145923.mlab.com:45923/recipes"
 os.environ["MONGO_DBNAME"] = "recipes"
-os.environ["SECRET_KEY"] = secret
 # Storing MySQL connection details in an environment variable
 os.environ["DB_USERNAME"] = 'ioan1997'
 os.environ["DB_HOST"] = "localhost"
