638fb68dcf7d7a4f74dedc0a96832a0706c6c3ae
Ioan Hadarean <hadareannelutu@yahoo.com>
2019-05-07 17:50:44 +0000

    Added new os.environ user string

--- env.py
+++ env.py
@@ -6,7 +6,7 @@ os.environ["SECRET_KEY"] = "HadareanIoan1403Goagl"
 os.environ["MONGO_URI"] = "mongodb://me:1403Goagl@ds145923.mlab.com:45923/recipes"
 os.environ["MONGO_DBNAME"] = "recipes"
 # Storing MySQL connection details in an environment variable
-os.environ["DB_USERNAME"] = 'ioan1997'
+os.environ["DB_USERNAME"] = '$C9_USER'
 os.environ["DB_HOST"] = "localhost"
 os.environ["DB_PASS"] =  ''
 os.environ["DB_NAME"] = 'flaskapp'
