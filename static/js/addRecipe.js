93f292bf9437f2b6091021dd7a8fdf3852cd3748
Ioan Hadarean <hadareannelutu@yahoo.com>
2019-05-07 08:15:39 +0000

    Fixed small bug with clearing add recipe form

--- static/js/addRecipe.js
+++ static/js/addRecipe.js
@@ -1,5 +1,5 @@
 // Cancel profile update when reset progress button is clicked
-let resetProgressButton = document.getElementsByClassName('reset-progress')[0];
+let resetProgressButton = document.getElementsByClassName('event-clear')[0];
 let addRecipe = document.getElementById('add-recipe');
 resetProgressButton.addEventListener('click', resetAddRecipeForm);
 
