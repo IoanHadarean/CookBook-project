2d13fea770ce5b230cbe213eddf83a9d9eaa9084
Ioan Hadarean <hadareannelutu@yahoo.com>
2019-05-11 13:22:02 +0000

    Added setUp function for unittests/added more test case scenrios

--- test_app.py
+++ test_app.py
@@ -3,66 +3,129 @@ from werkzeug.datastructures import ImmutableMultiDict
 import app, re
 from app import RegisterForm, EditForm
 
+""" Test App Class  for unitesting """
 class TestApp(unittest.TestCase):
-    def test_registerForm(self):
-        registerForm = RegisterForm(ImmutableMultiDict([('confirm', 'random'), ('username', 'J1995Doe'), ('password', 'random'), ('name', 'John Doe'), ('email', 'jdoe@yahoo.com')]))
-        assert isinstance(registerForm, object)
-        for k, v in vars(registerForm).items():
-            if (k == '_fields'):
-                for attr, value in vars(v).items():
-                    if attr == '_OrderedDict__map':
-                        for i in value.values():
-                            assert isinstance(i, object)
-            if k == '_errors':
-                self.assertEqual(v, None)
-            if k == 'name':
-                for attr, value in vars(v).items():
-                    if attr == 'data':
-                        self.assertEqual(value, 'John Doe')
-            if k == 'username':
-                for attr, value in vars(v).items():
-                    if attr == 'data':
-                        self.assertEqual(value, 'J1995Doe')
-            if k == 'email':
-                for attr, value in vars(v).items():
-                    if attr == 'data':
-                        self.assertEqual(value, 'jdoe@yahoo.com')
-            if k== 'confirm':
-                for attr, value in vars(v).items():
-                    if attr == 'data':
-                        self.assertEqual(value, 'random')
-            if k== 'password':
-                for attr, value in vars(v).items():
-                    if attr == 'data':
-                        self.assertEqual(value, 'random')
-    def test_editForm(self):
-        editForm = EditForm(ImmutableMultiDict([('about_me', 'I am Ioan'), ('name', 'Ioan'), ('email', 'ioan@yahoo.com')]))
-        assert isinstance(editForm, object)
-        for k, v in vars(editForm).items():
-            if (k == '_fields'):
-                for attr, value in vars(v).items():
-                    if attr == '_OrderedDict__map':
-                        for i in value.values():
-                            assert isinstance(i, object)
-            if k == '_errors':
-                self.assertEqual(v, None)
-            if k == 'name':
-                for attr, value in vars(v).items():
-                    if attr == 'data':
-                        self.assertEqual(value, 'Ioan')
-            if k == 'about_me':
-                for attr, value in vars(v).items():
-                    if attr == 'data':
-                        self.assertEqual(value, 'I am Ioan')
+    
+    
+    """ Set up the forms for testing """
+    def setUp(self):
+        
+       # Create form lists
+       self.profileImages = []
+       self.registerForms = []
+       self.updateForms = [] 
+        
+       # Create form objects
+       self.registerForm1 = RegisterForm(ImmutableMultiDict([('confirm', 'random'), ('username', 'J1995Doe'), ('password', 'random'), ('name', 'John Doe'), ('email', 'jdoe@yahoo.com')]))
+       self.registerForm2 = RegisterForm(ImmutableMultiDict([('confirm', 'password'), ('username', 'Ana1532'), ('password', 'random'), ('name', 'Ana Brooklyn'), ('email', 'anabrook89@yahoo.com')]))
+       self.updateForm1 = EditForm(ImmutableMultiDict([('about_me', 'I am John'), ('name', 'John Doe'), ('email', 'jdoe95@yahoo.com')]))
+       self.updateForm2 = EditForm(ImmutableMultiDict([('about_me', 'I am Ana'), ('name', 'Ana Brooklyn'), ('email', 'anabrook1489@yahoo.com')]))
+       self.profileImage1 = EditForm(ImmutableMultiDict([('picture', 'Group 6.png')]))
+       self.profileImage2 = EditForm(ImmutableMultiDict([('picture', 'avatar.png')]))
+
+       # Append objects to form lists
+       self.profileImages.append({'profileImage1' : self.profileImage1})
+       self.profileImages.append({'profileImage2' : self.profileImage2})
+       self.registerForms.append({'registerForm1' : self.registerForm1})
+       self.registerForms.append({'registerForm2' : self.registerForm2})
+       self.updateForms.append({'updateForm1': self.updateForm1})
+       self.updateForms.append({'updateForm2': self.updateForm2})
+   
+   
+   
+   
+    """ Additional function to run after tests have been finished """
+    def tearDown(self):
+       pass
+    
+    
+    
+    """" Test the registration form values """
+    def test_registerForms(self):
+        for self.item in self.registerForms:
+            for key, dictionary in self.item.items():
+                if key == 'registerForm1':
+                    assert isinstance(dictionary, object)
+                    for k, v in vars(dictionary).items():
+                        if k == '_fields':
+                            for attr, value in vars(v).items():
+                                if attr == '_OrderedDict__map':
+                                    for i in value.values():
+                                        assert isinstance(i, object)
+                        if k == '_errors':
+                            self.assertEqual(v, None)
+                        if k == 'name':
+                            for attr, value in vars(v).items():
+                                if attr == 'data':
+                                    self.assertEqual(value, 'John Doe')
+                                    self.assertNotEqual(value, 'J1995Doe')
+                        if k == 'username':
+                            for attr, value in vars(v).items():
+                                if attr == 'data':
+                                    self.assertEqual(value, 'J1995Doe')
+                                    self.assertNotEqual(value, 'John Doe')
+                        if k == 'email':
+                            for attr, value in vars(v).items():
+                                if attr == 'data':
+                                    self.assertEqual(value, 'jdoe@yahoo.com')
+                                    self.assertNotEqual(value, 'jdoe95@yahoo.com')
+                        if k == 'confirm':
+                            for attr, value in vars(v).items():
+                                if attr == 'data':
+                                    self.assertEqual(value, 'random')
+                                    self.assertNotEqual(value, 'randoms')
+                        if k == 'password':
+                            for attr, value in vars(v).items():
+                                if attr == 'data':
+                                    self.assertEqual(value, 'random')
+                                    self.assertNotEqual(value, 'randoms')
+            
+            
+                        
+    """ Test the edit form values """            
+    def test_updateForm(self):
+        for self.item in self.updateForms:
+             for key, dictionary in self.item.items():
+                 if key == 'updateForm1':
+                    assert isinstance(dictionary, object)
+                    for k, v in vars(dictionary).items():
+                        if k == '_fields':
+                            for attr, value in vars(v).items():
+                                if attr == '_OrderedDict__map':
+                                    for i in value.values():
+                                        assert isinstance(i, object)
+                        if k == '_errors':
+                            self.assertEqual(v, None)
+                        if k == 'name':
+                            for attr, value in vars(v).items():
+                                if attr == 'data':
+                                    self.assertEqual(value, 'John Doe')
+                                    self.assertNotEqual(value, 'Johnny Doe')
+                        if k == 'about_me':
+                            for attr, value in vars(v).items():
+                                if attr == 'data':
+                                    self.assertEqual(value, 'I am John')
+                                    self.assertNotEqual(value, 'I am Ioan')
+                        if k == 'email':
+                            for attr, value in vars(v).items():
+                                if attr == 'data':
+                                    self.assertEqual(value, 'jdoe95@yahoo.com')
+                                    self.assertNotEqual(value, 'jdoe@yahoo.com')
+           
+           
+                        
+    """ Test profile image from edit form """                    
     def test_profileImage(self):
-        profileImage = EditForm(ImmutableMultiDict([('picture', 'Group 6.png')]))
-        assert isinstance(profileImage, object)
-        for k, v in vars(profileImage).items():
-            if (k == '_fields'):
-                for attr, value in vars(v).items():
-                    if attr == '_OrderedDict__map':
-                        for i, j in value.items():
-                            if i == 'picture':
-                                assert isinstance(j, object)
-                                self.assertEqual(inspect.getmembers(j, predicate=inspect.ismethod), [])
+        for self.item in self.profileImages:
+            for key, dictionary in self.item.items():
+                if key == 'profileImage1':
+                    assert isinstance(dictionary, object)
+                    for k, v in vars(dictionary).items():
+                        if (k == '_fields'):
+                            for attr, value in vars(v).items():
+                                if attr == '_OrderedDict__map':
+                                    for i, j in value.items():
+                                        if i == 'picture':
+                                            assert isinstance(j, object)
+                                            self.assertEqual(inspect.getmembers(j, predicate=inspect.ismethod), [])
                             
