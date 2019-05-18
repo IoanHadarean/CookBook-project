053606a56412919bd4fcce0ccef88e61ea6cc201
Ioan Hadarean <hadareannelutu@yahoo.com>
2019-05-18 19:18:32 +0000

    Changed SQL connection strings

--- env.py
+++ env.py
@@ -6,7 +6,7 @@ os.environ["SECRET_KEY"] = "3markHero"
 os.environ["MONGO_URI"] = "mongodb://admin_recipe:3markHero@ds145923.mlab.com:45923/recipes"
 os.environ["MONGO_DBNAME"] = "recipes"
 # Storing MySQL connection details in an environment variable
-os.environ["DB_USERNAME"] = 'e7uhzupcv0npu7fp'
-os.environ["DB_HOST"] = 'wyqk6x041tfxg39e.chr7pe7iynqr.eu-west-1.rds.amazonaws.com'
-os.environ["DB_PASS"] =  'v7mib76bqqp4d3yu'
-os.environ["DB_NAME"] = 'rg0qy5ylfuoz0084'
+os.environ["DB_USERNAME"] = 'vpsf8o0ljbuwnws9'
+os.environ["DB_HOST"] = 'dz8959rne9lumkkw.chr7pe7iynqr.eu-west-1.rds.amazonaws.com'
+os.environ["DB_PASS"] =  'lqdqx2ayu621x9d9'
+os.environ["DB_NAME"] = 'k4c5aevnff06iuzj'
