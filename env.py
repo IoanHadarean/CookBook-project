a2f2cf9417a67daf1dedf4f0b983f25eece5d88e
Ioan Hadarean <hadareannelutu@yahoo.com>
2019-05-18 18:55:28 +0000

    Changed MongoURI

--- env.py
+++ env.py
@@ -1,9 +1,9 @@
 import os
 
 # Create a secret key
-os.environ["SECRET_KEY"] = "HadareanIoan1403Goagl"
+os.environ["SECRET_KEY"] = "3markHero"
 # Storing the MONGO_URI environment variable
-os.environ["MONGO_URI"] = "mongodb://me:1403Goagl@ds145923.mlab.com:45923/recipes"
+os.environ["MONGO_URI"] = "mongodb://admin_recipe:3markHero@ds145923.mlab.com:45923/recipes"
 os.environ["MONGO_DBNAME"] = "recipes"
 # Storing MySQL connection details in an environment variable
 os.environ["DB_USERNAME"] = 'e7uhzupcv0npu7fp'
